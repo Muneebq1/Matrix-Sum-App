e0939e6405c70d0680a2dfa7cf7df110e07dcc90
muneeb shahzad <muneebshahzad244@gmail.com>
2022-06-17 02:11:13 +0500

    hogya akhir kaar

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+let row = "";
+let col = "";
+
 function start() {
-    let row = Number(document.querySelector("#row_size").value);
-    let col = Number(document.querySelector("#col_size").value);
+    row = +document.querySelector("#row_size").value;
+    col = +document.querySelector("#col_size").value;
 
     let input1 = document.querySelector("#input_1");
     let input2 = document.querySelector("#input_2");
+    let mResult = document.querySelector("#mResult");
+
+    input1.innerHTML = "";
+    input2.innerHTML = "";
+    mResult.innerHTML = "";
 
     if (row !== col) {
         alert("Please enter the same number for both inputs");
@@ -20,33 +28,36 @@ function start() {
 
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
-            input1.innerHTML += `<input type="number" required min="0" max="100" id="matrix_value1_${i}${j}" value="0">`;
-            
-            input2.innerHTML += `<input type="number" required min="0" max="100" id="matrix_value2_${i}${j}" value="0">`;
+            input1.innerHTML += `<input type="number" required min="0" max="100" id="m1-${i}-${j}" value="0">`;
 
-            result.innerHTML += `<input type="number" disabled required min="0" max="100" id="matrix_Result-${i}-${j}" value="0">`;
+            input2.innerHTML += `<input type="number" required min="0" max="100" id="m2-${i}-${j}" value="0">`;
+
+            mResult.innerHTML += `<input type="number" disabled required min="0" max="100" id="mResult-${i}-${j}" value="0">`;
         }
         input1.innerHTML += `<br>`;
         input2.innerHTML += `<br>`;
-        result.innerHTML += `<br>`;
+        mResult.innerHTML += `<br>`;
+
+        
     }
     document.querySelector("#form2").classList.remove("hidden");
+
 }
 
-function sum() { 
-    let value1 = Number(document.querySelector(`#matrix_value1_${i}${j}`).value);
-    let value2 = Number(document.querySelector(`#matrix_value2_${i}${j}`).value);
-    let value3 = Number(document.querySelector(`#matrix_Result-${i}${j}`).value);
-    result = document.querySelector("#result");
-    
+
+function sum() {
+
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
-           
-            value3 = value1 + value2;
-           
-            result.innerHTML += value3;
-        }
+            console.log("running");
 
+            document.querySelector(`#mResult-${i}-${j}`).value =
+                +document.querySelector(`#m1-${i}-${j}`).value
+                +
+                +document.querySelector(`#m2-${i}-${j}`).value;
+        }
     }
 
+
 }
+
